5b52c14d41c33985af4731fd08c4f6671d5a6ca0
georges.rached@epitech.eu <georges.rached@epitech.eu>
2019-02-24 18:03:32 +0100

    did modification on basic components

--- components/include/4011.hpp
+++ components/include/4011.hpp
@@ -8,28 +8,28 @@
 #if !defined(_4011_HPP_)
 #define _4011_HPP_
 
+#include <map>
+#include <functional>
 #include "IComponent.hpp"
 
 class CMP4011: public nts::IComponent
 {
-    public:
-        CMP4011(std::string name);
-        std::string getName();
-        Pins **getPins();
-        Gates **getGates();
-        int countPins();
-        int countGates();
-        Pins *findPin(std::size_t id);
-        Gates *findGate(size_t id);
-        nts::Tristate compute(std::size_t pin = 1);
-        void setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin);
+        public:
+                CMP4011(std::string name);
+                virtual ~CMP4011();
+                std::string getName() const;
+                nts::Tristate compute(std::size_t pin = 1);
+                void setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin);
 
-    private:
-        int _gates_nb;
-        int _pins_nb;
-        std::string _name;
-        Pins *pins_array[12];
-        Gates *gates_array[4];
+        private:
+                std::string _name;
+                std::map<std::size_t, std::pair<nts::IComponent *, std::size_t> > _links;
+                std::map<std::size_t, std::pair<std::size_t, std::size_t> > _gates;
+                std::map<std::size_t, std::function<nts::Tristate(std::size_t)> > _pin_func;
+
+                nts::Tristate inputPin(std::size_t pin);
+                nts::Tristate outputPin(std::size_t pin);
+                nts::Tristate andCompute(std::size_t a, std::size_t b) const;
 };
 
 #endif
